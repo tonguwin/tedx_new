e7491dc455ba983d74ea6151c25e9111f9358a21
tonguwin <ttn135@ucsd.edu>
2021-05-10 15:15:08 -0700

    Andy Nahas update

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -13,6 +13,7 @@ import Grad from './Images/grad1.png'
 import Lib from './Images/library.png'
 import Rady from './Images/rady1.png'
 import Research from './Images/research1.png'
+import Andy from './Images/andy.jpeg'
 
 // ALTHOUGH THIS PAGE IS CALLED CONTACT -- It's Sponsors
 
@@ -36,6 +37,35 @@ class Contact extends Component {
                 <Row>
                     <p className="text2">Our organization wouldn't be possible without the support and encouragement of our wonderful sponsors! If your company or department is interested in sponsoring us and learning more information about our Sponsor Membership program, please contact us at <a href="mailto:tedxfinance@gmail.com">tedxfinance@ucsd.edu</a></p>
                 </Row>
+
+                
+
+                <Container style={{paddingTop:"6rem"}}>
+                    <Row>
+                        <Col>
+                            <a href="http://www.prospectfund.com/" target="_blank">
+                                <img src={Andy} className="image zoom"/>
+                                </a>
+                        </Col>
+                        <Col style={{paddingLeft:"5rem"}}>
+                            <p className="meet2">
+                                Andy Nahas 
+                            </p>
+                            <p className="meet3">
+                                2021 | Platinum Sponsor
+                            </p>
+                            <p className="text3">
+                            Andy Nahas is a UC San Diego alum who founded <a href="http://www.prospectfund.com/" target="_blank">The Prospect Fund</a>, a socially responsible investment business which makes money in the stock market and uses profits to support social justice causes.  The Prospect Fund is the top performing fund in the BarclayHedge database for the most recent 25 year period.  
+                            </p>
+                            <p className="text3" style={{paddingTop:"2rem"}}>
+                            Andy also founded <a href="https://www.musicpower.org/" target="_blank">MusicPower</a>, a charity which supports exciting initiatives that empower people through music.  He created this organization after witnessing the power of music while performing professional style concerts at nursing homes for over 25 years.  His first audience ever was in a Third College talent show at UCSD. 
+                            </p>
+                        </Col>
+                    </Row>
+
+                   
+                </Container>
+
                 <Row style={{paddingTop:"2rem"}}>
                     <p className="meet">
                         Past Sponsors
